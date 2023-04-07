9f1cf21b1d04108023691d880c5b6773de3a48e6
Shashwat Garg <shashwatg2002@gmail.com>
2023-04-07 15:59:41 +0530

    Basic template, will discuss structure with all

--- Assignment_3/contracts/Payment.sol
+++ Assignment_3/contracts/Payment.sol
@@ -1,5 +1,27 @@
 pragma solidity ^0.8.0;
 
 contract Payment {
-    string public message = "Hello World!";
+
+    mapping(address => uint256) public balances;
+
+    // function to initialise the graph
+    // other helper functions
+
+    function registerUser (use id, user name) public {
+        
+    }
+
+    function createAcc(user id 1, user id 2) public {
+
+    }
+
+    function sendAmount(user id 1, user id 2) public {
+
+
+    }
+
+    function closeAccount(user id 1, user id 2) public {
+
+    }
+
 }
