cc759bc44229e93c9bae16fc100fb389ed1444da
Shashwat Garg <shashwatg2002@gmail.com>
2023-04-09 09:00:05 +0530

    Handle Gas limits, all done

--- Assignment_3/client.py
+++ Assignment_3/client.py
@@ -1,6 +1,8 @@
 import json
 from web3 import Web3
-
+import networkx as nx
+import numpy as np
+import pickle as pkl
 
 #connect to the local ethereum blockchain
 provider = Web3.HTTPProvider('http://127.0.0.1:8545')
@@ -9,7 +11,7 @@ w3 = Web3(provider)
 print(w3.is_connected())
 
 #replace the address with your contract address (!very important)
-deployed_contract_address = '0x5eA7D7c544df13DBda9c12FF8B4b875dAde57E2c'
+deployed_contract_address = '0xcf4AFc44737c95637cAEf36eDBcBD9191dFCeD7F'
 
 #path of the contract json file. edit it with your contract json file
 compiled_contract_path ="build/contracts/Payment.json"
@@ -34,3 +36,60 @@ print(w3.eth.get_transaction(txn_receipt_json))
 '''
 
 #Add your Code here
+
+n = 100
+m = 3
+p = 0.5
+gas_limit = 1000000
+
+power_graph = nx.powerlaw_cluster_graph(n,m,p)
+
+# Check if the graph is connected
+while not nx.is_connected(power_graph):
+    power_graph = nx.powerlaw_cluster_graph(n,m,p)
+
+adj_list = nx.to_numpy_array(power_graph)
+
+# Print the adjacency list
+print(adj_list.shape)
+
+for i in range(1,n+1):
+    txn_receipt = contract.functions.registerUser(i,str(i)).transact({'from':w3.eth.accounts[0], 'gas':gas_limit})
+    txn_receipt_json = json.loads(w3.to_json(txn_receipt))
+    print("Creating Node",i,"Hash", txn_receipt_json) # print transaction hash
+    
+for i in range(1,n+1):
+    for j in range(i+1,n+1):
+        if adj_list[i-1][j-1]:
+            mean = 10
+            amount = round(np.random.exponential(mean))
+            txn_receipt = contract.functions.createAcc(i,j,amount).transact({'from':w3.eth.accounts[0], 'gas':gas_limit})
+            txn_receipt_json = json.loads(w3.to_json(txn_receipt))
+            print("Creating Account",i,j,"Amount",amount,"Hash", txn_receipt_json) # print transaction hash
+
+num_txns = 1000
+success_arr = np.zeros(num_txns)
+for txn in range(num_txns):
+    i,j = np.random.choice(range(1,n+1),2,replace=False)
+    txn_receipt = contract.functions.sendAmount(int(i),int(j)).transact({'from':w3.eth.accounts[0], 'gas':gas_limit})
+    txn_receipt_json = json.loads(w3.to_json(txn_receipt))
+    print("Send Amount",i,j,"Hash",txn_receipt_json,"Status",contract.events.TxnStatus.get_logs()[0].args['status']) # print transaction hash
+    success_arr[txn] = contract.events.TxnStatus.get_logs()[0].args['status']
+
+print(success_arr)
+
+with open('results.pkl','wb') as f:
+    pkl.dump(success_arr,f)
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Assignment_3/contracts/Payment.sol
+++ Assignment_3/contracts/Payment.sol
@@ -6,40 +6,91 @@ contract Payment {
         uint id;
         string name;
     }
-    
+    event TxnStatus(bool status);
+
+    uint num_users = 0;
+    mapping(uint => bool) public visited;
+    mapping(uint => uint) public parent;
+    mapping(uint => uint) public keys_list;
     mapping(uint => User) public user_list;
     mapping(uint => mapping(uint => uint)) public jointAccount;
     //index i, j indicates balance of i in the account for i<->j
     
     function registerUser(uint userId, string memory name) public {
-        require(user_list[userId].id==0, "User already exists");
+        require(user_list[userId].id==0, "Error - User already exists");
         user_list[userId] = User(userId, name);
+        keys_list[num_users++] = userId;
     }
     
     function createAcc(uint userId1, uint userId2, uint amount) public {
-        require(user_list[userId1].id!=0, "User 1 does not exist");
-        require(user_list[userId2].id!=0, "User 2 does not exist");
+        require(user_list[userId1].id!=0, "Error - User 1 does not exist");
+        require(user_list[userId2].id!=0, "Error - User 2 does not exist");
         require(jointAccount[userId1][userId2] == 0, "Account already exists");
 
         jointAccount[userId1][userId2] = amount/2;
         jointAccount[userId2][userId1] = amount/2;
     }
     
-    function sendAmount(uint userId1, uint userId2, uint amount) public {
-        require(user_list[userId1].id!=0, "User 1 does not exist");
-        require(user_list[userId2].id!=0, "User 2 does not exist");
-        require(jointAccount[userId1][userId2] >= amount, "Insufficient balance for User 1");
+    function sendAmount(uint userId1, uint userId2) public {
+        require(user_list[userId1].id!=0, "Error - User 1 does not exist");
+        require(user_list[userId2].id!=0, "Error - User 2 does not exist");
 
-        //need to add BFS code here...
+        for(uint i=0;i<num_users;i++){
+            visited[keys_list[i]] = false;
+            parent[keys_list[i]] = 0;
+        }
 
+        uint[] memory queue = new uint[](num_users);
+        queue[0] = userId1;
+        visited[userId1] = true;
+        parent[userId1] = 0;
 
-        jointAccount[userId1][userId2] -= amount;
-        jointAccount[userId2][userId1] += amount;
+        uint head = 0;
+        uint tail = 1;
+
+        while(head<tail){
+            uint node = queue[head];
+            head++;
+
+            for (uint i = 0; i < num_users; i++) {
+                uint neighbor = keys_list[i];
+                if (jointAccount[node][neighbor]+jointAccount[neighbor][node]>0 && !visited[neighbor]) {
+                    visited[neighbor] = true;
+                    parent[neighbor] = node;
+                    queue[tail] = neighbor;
+                    tail++;
+                }
+            }
+        }
+
+        uint node1 = parent[userId2];
+        uint node2 = userId2;
+
+        while(node2!=userId1){
+            if(jointAccount[node1][node2]<1){
+                emit TxnStatus(false);
+                return;   
+            }
+            node2 = node1;
+            node1 = parent[node2];
+        }
+
+        node1 = parent[userId2];
+        node2 = userId2;
+
+        while(node2!=userId1){
+            jointAccount[node1][node2] -= 1;
+            jointAccount[node2][node1] += 1;
+            node2 = node1;
+            node1 = parent[node2];
+        }
+        emit TxnStatus(true);
+
     }
     
     function closeAccount(uint userId1, uint userId2) public {
-        require(user_list[userId1].id!=0, "User 1 does not exist");
-        require(user_list[userId2].id!=0, "User 2 does not exist");
+        require(user_list[userId1].id!=0, "Error - User 1 does not exist");
+        require(user_list[userId2].id!=0, "Error - User 2 does not exist");
 
         jointAccount[userId1][userId2] = 0;
         jointAccount[userId2][userId1] = 0;
